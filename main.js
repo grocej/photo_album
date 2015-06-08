0fc744d8f0f1308d5950ae1b95b912fefdec47a9
Mac User <MacBookAir@Macs-MacBook-Air.local>
2015-06-08 14:52:44 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+//Hide albums until they are clicked
+$(".indivContain").hide();
+//Add a click event to a button
+
+$(".album1").on('click', 'threePage', $target) {
+  event.preventDefault();
+  var clickedPage = $(this).attr.('rel');
+  console.log("clicked")
+  $(".nav").hide();
+
+};
+
+});
+
+$(".album2").click(function(event){
+    event.preventDefault();
+    $(".firstRow").hide();
+
+    $(".secondRow").hide();
+
+    $("#gainesPage").show();
+
+});
+
+//When clicked show the album clicked
+//And Hide the button album
+//Make a back to albums button appear
+//Make button hide all but current album
+});
+
+$(".buttonRow").hide();
+
+
+
+$(".albums").hide();
+//Add a click event to a button
+//When clicked show the album clicked
+//And Hide the button bar
+//Make a back to albums button appear
+//Make button hide all but current album
--- map-filter-fp/main.js
+++
@@ -1,5 +0,0 @@
-console.log("Your code goes in this document.");
-
-_.times(100, function() {
-  console.log("hi");
-});
